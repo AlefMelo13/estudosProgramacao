81fa411b196bd102cc6baead64ad8d6da0be2cbf
alefmelo <alefmelo13@gmail.com>
2022-06-15 18:47:10 -0300

    cadastro de produto

---
+++ OrientacaoObjeto/CadastroProduto/CadastroProduto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace CadastroProduto
+{
+    class CadastroProduto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- CADASTRO DE PRODUTO ---------");
+
+            Produto produto = new Produto();
+
+            Console.WriteLine("Digite as informações do produto");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados cadastrados com sucesso!");
+            Console.WriteLine();
+
+            Console.WriteLine($"Nome do produto: {produto.Nome}");
+            Console.WriteLine("Preço do produto: " + produto.Preco.ToString("F2"));
+            Console.WriteLine($"Quantidade do produto: {produto.Quantidade}");
+
+            Console.WriteLine("O valor total do estoque do produto é: " + produto.ValorTotalEstoque().ToString("F2"));
+        }
+    }
+}
---
+++ OrientacaoObjeto/OrientacaoObjeto/AreaTriangulo.cs
@@ -0,0 +1,46 @@
+﻿using AreaTriangulo;
+using System;
+using System.Globalization;
+
+namespace AraTriangulo
+{
+    class AreaTriangulo
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            double areaX, areaY;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite as medidas do triangulo X: ");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite as medidas do triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+
+            areaX = x.Area();
+
+            areaY = y.Area();
+
+            Console.WriteLine("A área do triangulo X é: " + areaX.ToString("F4"));
+
+            Console.WriteLine("A área do triangulo Y é: " + areaY.ToString("F4"));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine($"A maior área é X: {areaX}");
+            }
+            else
+            {
+                Console.WriteLine($"A maior área é Y: {areaY}");
+            }
+        }
+    }
+}
