94592388d3f32521f21ffb75010967ec5aaec3bd
alefmelo <alefmelo13@gmail.com>
2022-06-17 15:04:19 -0300

    adicionado projeto aumento de salario funcionario

---
+++ OrientacaoObjeto/AumentoFuncionario/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AumentoFuncionario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+        public double Porcentagem;
+
+        public override string ToString()
+        {
+            return Nome + ", R$" + SalarioLiquido().ToString("F2");
+        }
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void Aumento(double porcentagemAumento)
+        {
+            SalarioBruto = ((porcentagemAumento / 100) * SalarioBruto) + SalarioBruto;
+        }
+    }
+}
