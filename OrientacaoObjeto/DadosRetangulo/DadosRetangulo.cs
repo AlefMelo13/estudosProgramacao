145692c7857d5ff468b5bfdd304034b4b8194e2e
alefmelo <alefmelo13@gmail.com>
2022-06-17 13:20:13 -0300

    adicionado exercicio dados retangulo

---
+++ OrientacaoObjeto/DadosRetangulo/DadosRetangulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace DadosRetangulo
+{
+    class DadosRetangulo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- DADOS RETÂNGULO ----------");
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Digite os dados do retângulo:");
+            Console.Write("Base: ");
+            retangulo.Base = double.Parse(Console.ReadLine());
+            Console.Write("Altura: ");
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do retângulo é: " + retangulo.Area().ToString("F2"));
+
+            Console.WriteLine("O perimetro do retângulo é: " + retangulo.Perimetro().ToString("F2"));
+
+            Console.WriteLine("A diagonal do retângulo é: " + retangulo.Diagonal().ToString("F2"));
+        }
+    }
+}
---
+++ OrientacaoObjeto/DadosRetangulo/Retangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DadosRetangulo
+{
+    internal class Retangulo
+    {
+        public double Base;
+        public double Altura;
+
+        //RETORNA A ÁREA DO RETÂNGULO
+        public double Area()
+        {
+
+            return Base * Altura;
+        }
+
+        //RETORNA O PERÍMETRO DO RETÂNGULO
+        public double Perimetro()
+        {
+            return 2 * (Base + Altura);
+        }
+
+        //RETORNA A DIAGONAL DO RETÂNGULO
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Base, 2) + Math.Pow(Altura, 2));
+        }
+    }
+}
