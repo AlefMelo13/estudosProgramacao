5b93bbacddc71f10668e802c6ddd1825496e928c
alefmelo <alefmelo13@gmail.com>
2022-06-17 17:34:11 -0300

    projeto status aluno

---
+++ OrientacaoObjeto/StatusAluno/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusAluno
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double Nota01, Nota02, Nota03;
+
+        public double NotaFinal()
+        {
+            return Nota01 + Nota02 + Nota03;
+        }
+        public double RestoNota()
+        {
+            return 60.0 % NotaFinal();
+        }
+    }
+}
---
+++ OrientacaoObjeto/StatusAluno/StatusAluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace StatusAluno
+{
+    class StatusAluno
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- STATUS ALUNO ----------");
+
+            Aluno aluno = new Aluno();
+
+            //ENTRADA DE DADOS DO ALUNO
+            Console.WriteLine("Digite os dados do aluno:");
+            Console.WriteLine();
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Agora entre com as notas do aluno");
+            Console.Write("Nota01: ");
+            aluno.Nota01 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota02: ");
+            aluno.Nota02 = double.Parse(Console.ReadLine());
+
+            Console.Write("Nota03: ");
+            aluno.Nota03 = double.Parse(Console.ReadLine());
+
+            //RETORNA A NOTA FINAL DO ANO
+            Console.WriteLine("Nota Final: " + aluno.NotaFinal().ToString("F2"));
+
+            //VERIFICA STATUS DO ALUNO
+            if (aluno.NotaFinal() > 60)
+            {
+                Console.WriteLine("ALUNO APROVADO!");
+            }
+            else
+            {
+                Console.WriteLine("ALUNO REPROVADO");
+                Console.WriteLine("Faltam " + aluno.RestoNota() + " pontos para ser aprovado");
+            }
+        }
+    }
+}
