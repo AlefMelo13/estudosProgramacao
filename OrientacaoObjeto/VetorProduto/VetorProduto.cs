705001c291ac23bf92382f45b4c4e1e29c6b801a
Alef Melo <alefmelo13@gmail.com>
2022-06-23 20:02:51 -0300

    deletados projetos não criados e acrescentado cw em projeto vetorcalculadora

--- OrientacaoObjeto/VetorProduto/VetorProduto.cs
+++ OrientacaoObjeto/VetorProduto/VetorProduto.cs
@@ -6,24 +6,25 @@
         {
             Console.WriteLine("---------- CADASTRO DE PRODUTO E PREÇO ----------");
 
-            Console.WriteLine("Digite a quantidade de produto a ser cadastrada: ");
+            Console.Write("Digite a quantidade de produto a ser cadastrada: ");
             int n = int.Parse(Console.ReadLine());
 
             Produto[] produto = new Produto[n];
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Digite o nome do produto: ");
+                Console.Write("Digite o nome do produto: ");
                 string nome = Console.ReadLine();
-                Console.WriteLine("Digite o preço do produto: ");
+                Console.Write("Digite o preço do produto: ");
                 double preco = double.Parse(Console.ReadLine());
 
                 produto[i] = new Produto { Nome = nome, Preco = preco };
             }
 
+            Console.WriteLine("Produtos cadastrados: ");
             for (int i =0; i < n; i++)
             {
-                Console.WriteLine(produto[i]);
+                Console.WriteLine(produto[i].Nome + " " + produto[i].Preco.ToString("F2"));
             }
 
         }
