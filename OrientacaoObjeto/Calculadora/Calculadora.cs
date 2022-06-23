efab47c1811c278135e978c0d1b977cda7e074a5
Alef Melo <alefmelo13@gmail.com>
2022-06-23 20:19:20 -0300

    iniciado projeto calculadora com classes

---
+++ OrientacaoObjeto/Calculadora/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculadora
+{
+    internal class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------- CALCUALDORA GERAL ----------");
+
+            Console.WriteLine("Qual operação você quer fazer?");
+            Console.Write("1-Soma, 2-Subtração, 3-Divisão ou 4-Multiplicação: ");
+            int operacao = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (operacao == 1)
+            {
+
+            }
+
+        }
+    }
+}
---
+++ OrientacaoObjeto/Calculadora/Calculo.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculadora
+{
+    internal class Calculo
+    {
+        public int Numero { get; set; }
+        public int Soma { get; set; }
+        public int Subtracao { get; set; }
+        public int Divisao { get; set; }
+        public int Mulriplicacao { get; set; }
+
+        public void SetSoma(int numero)
+        {
+            int[] soma = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                soma[i] = numero * i;
+            }
+
+
+        }
+
+    }
+}
