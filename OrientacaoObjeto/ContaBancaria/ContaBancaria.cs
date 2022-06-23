3b383e80d7a382055d370673552ca7dc20e5f84c
alefmelo <alefmelo13@gmail.com>
2022-06-23 17:49:01 -0300

    refatora nomes de parametros

--- OrientacaoObjeto/ContaBancaria/ContaBancaria.cs
+++ OrientacaoObjeto/ContaBancaria/ContaBancaria.cs
@@ -9,63 +9,51 @@ namespace ContaBancaria
         {
             Console.WriteLine("---------- CONTA BANCÁRIA ----------");
 
+            Conta conta;
+
             Console.Write("Digite o número da conta: ");
             double numeroConta = double.Parse(Console.ReadLine());
 
             Console.Write("Digite o nome do titular da conta: ");
-            string cliente = Console.ReadLine();
+            string titular = Console.ReadLine();
 
             Console.Write("Haverá depósito inicial(s/n): ");
             string resposta = Console.ReadLine();
 
             //ESTRUTURA DE CONDIÇÃO PARA DEPÓSITO E SAQUE
-            if (resposta == "s")
+            if (resposta == "s" || resposta == "S")
             {
                 Console.Write("Digite o valor a ser depositado: ");
                 double valorDeposito = double.Parse(Console.ReadLine());
-                Conta conta = new Conta(cliente, numeroConta, valorDeposito);
-                Console.WriteLine();
-                Console.WriteLine("Dados da conta: " + conta);
-
-                //DEPÓTITO DE DINHEIRO
-                Console.WriteLine();
-                Console.Write("Digite um valor para depósito: ");
-                double deposito = double.Parse(Console.ReadLine());
-                conta.SetDeposito(deposito);
-
-                Console.WriteLine("Dados da conta atualizados: " + conta);
-
-                //SAQUE DE DINHEIRO
-                Console.WriteLine();
-                Console.Write("Digite um valor para saque: ");
-                double saque = double.Parse(Console.ReadLine());
-                conta.SetSaque(saque);
-
-                Console.WriteLine();
-                Console.WriteLine("Dados da conta atualizados: " + conta);
+                conta = new Conta(titular, numeroConta, valorDeposito);
             }
             else
             {
-                Conta conta = new Conta(cliente, numeroConta);
-                Console.WriteLine(conta);
-
-                //DEPÓSITO DE DINHEIRO
-                Console.WriteLine();
-                Console.Write("Digite um valor para depósito: ");
-                double deposito = double.Parse(Console.ReadLine());
-                conta.SetDeposito(deposito);
-                Console.WriteLine();
-                Console.WriteLine("Dados da conta atualizados: " + conta);
-
-                //SAQUE DE DINHEIRO
-                Console.WriteLine();
-                Console.Write("Digite um valor para saque: ");
-                double saque = double.Parse(Console.ReadLine());
-                conta.SetSaque(saque);
-
-                Console.WriteLine();
-                Console.WriteLine("Dados da conta atualizados: " + conta);
+                conta = new Conta(titular, numeroConta);
             }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            //DEPÓTITO DE DINHEIRO
+            Console.WriteLine();
+            Console.Write("Digite um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.SetDeposito(deposito);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            //SAQUE DE DINHEIRO
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine());
+            conta.SetSaque(saque);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
         }
     }
 }
