348f8327cb59bd221f8dac8971f1c7f8903038f3
Alef Melo <alefmelo13@gmail.com>
2022-06-27 20:13:17 -0300

    pensionato com vetor e classe

---
+++ OrientacaoObjeto/Pensaionato/Inquilino.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pensaionato
+{
+    internal class Inquilino
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int NumeroQuarto { get; set; }
+    }
+}
