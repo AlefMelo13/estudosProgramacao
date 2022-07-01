9da6fedef3510f63421b7c2c1de857fb7f0acea9
alefmelo <alefmelo13@gmail.com>
2022-07-01 17:46:35 -0300

    projeto membro de instituicao - aula lista

---
+++ OrientacaoObjeto/MembroInstituicao/Membro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembroInstituicao
+{
+    internal class Membro
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Telefone { get; set; }
+
+        //CONSTRUTORES
+        public Membro(int codigo, string nome, int idade, string endereco, int numero, string bairro, string cidade, string estado, string telefone)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Idade = idade;
+            Endereco = endereco;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;
+            Telefone = telefone;
+        }
+
+        public override string ToString()
+        {
+            return "Código: " + Codigo +
+                   "Nome: " + Nome +
+                   "Idade: " + Idade +
+                   "Endereço: " +Endereco +
+                   "Bairro: " + Bairro +
+                   "Cidade: " + Cidade +
+                   "Estado :" + Estado +
+                   "Telefone: " + Telefone;
+        }
+    }
+}
