a95623d3b72075b3aee267d85d1860379b876d77
alefmelo <alefmelo13@gmail.com>
2022-07-05 13:12:19 -0300

    iniciado o projeto programa pedidos da aula de enums

---
+++ ProgramaPedido/ProgramaPedido/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using ProgramaPedidos.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaPedidos.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
--- ProgramaPedido/ProgramaPedido/Program.cs
+++ ProgramaPedido/ProgramaPedido/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ProgramaPedido
+﻿using ProgramaPedidos.Entities;
+using ProgramaPedidos.Entities.Enums;
+
+namespace ProgramaPedidos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
         }
     }
 }
