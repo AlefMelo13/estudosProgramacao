c4b660aa4f2043a836146b02baaeaa7b7cd0be0b
alefmelo <alefmelo13@gmail.com>
2022-07-05 18:54:01 -0300

    adicionados parâmetros projet principal - contrato trabalhadores

--- ContratosTrabalhadores/ContratosTrabalhadores/Program.cs
+++ ContratosTrabalhadores/ContratosTrabalhadores/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ContratosTrabalhadores
+﻿using ContratosTrabalhadores.Entities;
+
+namespace ContratosTrabalhadores
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Digite o departamento: ");
+            string departamento = Console.ReadLine();
+
+            Console.WriteLine("Digite os dados do trabalhador:");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Nível :");
+            string nivel = Console.ReadLine();
+
+            Console.WriteLine("Salário Base: ");
+            double salarioBase = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos contratos o funcionário tem? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+
+                List<HoraContrato> contrato = new List<HoraContrato>();
+            }
         }
     }
 }
