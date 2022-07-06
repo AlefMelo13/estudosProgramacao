dcfbc463b967aa7495c5cc0f3606ec93e958e6f5
alefmelo <alefmelo13@gmail.com>
2022-07-06 16:55:51 -0300

    criado projeto próprio - instituição

---
+++ Instituicao/Instituicao/Entities/CadastroInstituicao.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instituicao.Entities
+{
+    class CadastroInstituicao
+    {
+        public string CnpjCpf { get; set; }
+        public string RazaoSocial { get; set; }
+        public string NomeFantasia { get; set; }
+        List<EnderecoInstituicao> Endereco { get; set; } = new List<EnderecoInstituicao>();
+        List<Membros> Membro { get; set; } = new List<Membros>();
+
+        //CONSTRUTORES
+        public CadastroInstituicao()
+        {
+        }
+
+        public CadastroInstituicao(string cnpjCpf, string razaoSocial, string nomeFantasia)
+        {
+            CnpjCpf = cnpjCpf;
+            RazaoSocial = razaoSocial;
+            NomeFantasia = nomeFantasia;
+        }
+
+        public CadastroInstituicao(string cnpjCpf, string razaoSocial, string nomeFantasia, List<EnderecoInstituicao> endereco)
+        {
+            CnpjCpf = cnpjCpf;
+            RazaoSocial = razaoSocial;
+            NomeFantasia = nomeFantasia;
+            Endereco = endereco;
+        }
+
+        //MÉTODO PARA ADICIONAR ENDEREÇO NA CLASSE
+        public void AdicionaEndereco(EnderecoInstituicao endereco)
+        {
+            Endereco.Add(endereco);
+        }
+
+        //MÉTODO PARA REMOVER ENDEREÇO DA CLASSE
+        public void RemoveEndereco(EnderecoInstituicao endereco)
+        {
+            Endereco.Remove(endereco);
+        }
+
+        //MÉTODO PARA ADICIONAR MEMBRO NA CLASSE
+        public void AdicionaMembro(Membros membro)
+        {
+            Membro.Add(membro);
+        }
+
+        //MÉTODO PARA REMOVER MEMBRO DA CLASSE
+        public void RemoveMembro(Membros membro)
+        {
+            Membro.Remove(membro);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder inst = new StringBuilder();
+            inst.AppendLine(CnpjCpf.ToString());
+            inst.AppendLine(RazaoSocial);
+            inst.AppendLine(NomeFantasia);
+
+            return inst.ToString();
+        }
+    }
+}
---
+++ Instituicao/Instituicao/Program.cs
@@ -0,0 +1,118 @@
+﻿using Instituicao.Entities;
+
+namespace Instituicao
+{
+    internal class Program
+    {
+        private static IEnumerable<Membros> membro;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- CADASTRO DE INSTITUIÇÃO E MEMBROS ----------");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite os dados da instituição:");
+            Console.Write("Cnpj ou Cpf: ");
+            string cnpjCpf = Console.ReadLine();
+
+            Console.Write("Razão Social: ");
+            string razao = Console.ReadLine();
+
+            Console.Write("Nome Fantasia: ");
+            string fantasia = Console.ReadLine();
+
+            Console.Write("Rua: ");
+            string rua = Console.ReadLine();
+
+            Console.Write("Número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Complemento: ");
+            string complemento = Console.ReadLine();
+
+            Console.Write("Bairro: ");
+            string bairro = Console.ReadLine();
+
+            Console.Write("Cidade: ");
+            string cidade = Console.ReadLine();
+
+            Console.Write("Estado: ");
+            string estado = Console.ReadLine();
+
+            Console.Write("Telefone: ");
+            string telefone = Console.ReadLine();
+
+            EnderecoInstituicao enderecoInstituicao = new EnderecoInstituicao(rua, numero, complemento, bairro, cidade, estado, telefone);
+
+            CadastroInstituicao instituicao = new CadastroInstituicao(cnpjCpf, fantasia, razao);
+
+            Console.Write("Deseja adicionar membros a instituição(s/n)? ");
+            string resposta = Console.ReadLine();
+
+            if (resposta == "s" || resposta == "S")
+            {
+                Console.Write("Quantos membros deseja adicionar? ");
+                int n = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+
+                    Console.Write("Sobrenome: ");
+                    string sobrenome = Console.ReadLine();
+
+                    Console.Write("Idade: ");
+                    int idade = int.Parse(Console.ReadLine());
+
+                    Console.Write("Sexo: ");
+                    string sexo = Console.ReadLine();
+
+                    Console.Write("Rua: ");
+                    string ruaMembro = Console.ReadLine();
+
+                    Console.Write("Número: ");
+                    int numeroMembro = int.Parse(Console.ReadLine());
+
+                    Console.Write("Complemento: ");
+                    string complementoMembro = Console.ReadLine();
+
+                    Console.Write("Bairro: ");
+                    string bairroMembro = Console.ReadLine();
+
+                    Console.Write("Cidade: ");
+                    string cidadeMembro = Console.ReadLine();
+
+                    Console.Write("Estado: ");
+                    string estadoMembro = Console.ReadLine();
+
+                    Console.Write("Telefone: ");
+                    string telefoneMembro = Console.ReadLine();
+
+                    EnderecoMembro enderecoMembro = new EnderecoMembro(ruaMembro, numeroMembro, complementoMembro, bairroMembro, cidadeMembro, estadoMembro, telefoneMembro);
+
+                    Membros membro = new Membros(nome, sobrenome, idade, sexo);
+
+                    instituicao.AdicionaMembro(membro);
+
+                    membro.AdicionaEndereco(enderecoMembro);
+                }
+
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da instituição:");
+            Console.WriteLine(instituicao.ToString());
+            Console.WriteLine(enderecoInstituicao);
+            Console.WriteLine();
+
+            Membros membro = new Membros();
+
+            Console.WriteLine("Membros da instituição:");
+            foreach(Membros m in membro)
+            {
+                Console.WriteLine(m.ToString());
+            }
+        }
+    }
+}
