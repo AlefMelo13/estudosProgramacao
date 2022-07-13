3f50f2075dbe9064b2025d01c12361a9f75ef794
Alef Melo <alefmelo13@gmail.com>
2022-07-13 20:26:16 -0300

    tarefa do curso - aula herança e polimorfismo

---
+++ EmpresaFuncionario/EmpresaFuncionario/Entities/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿namespace EmpresaFuncionario.Entities
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public int HorasTrabalhadas { get; set; }
+        public double ValorHora { get; set; }
+
+        public Funcionario(string nome, int horasTrabalhadas, double valorHora)
+        {
+            Nome = nome;
+            HorasTrabalhadas = horasTrabalhadas;
+            ValorHora = valorHora;
+        }
+
+        public virtual double Pagamento()
+        {
+            return HorasTrabalhadas * ValorHora;
+        }
+    }
+}
