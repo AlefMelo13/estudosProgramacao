922f03c9526552571f3fcad142c4b725891455fe
Alef Melo <alefmelo13@gmail.com>
2022-07-15 21:31:11 -0300

    exercicio proposto - produtos - heranca e polimorfismo - lista

---
+++ EtiquetaProduto/EtiquetaProduto/Entities/ProdutoUsado.cs
@@ -0,0 +1,18 @@
+﻿namespace EtiquetaProduto.Entities
+{
+    class ProdutoUsado : Produto
+    {
+        public DateTime DataFabricacao { get; set; }
+
+        public ProdutoUsado (string nomeProduto, double precoProduto, DateTime dataFabricacao) : base (nomeProduto, precoProduto)
+        {
+            DataFabricacao = dataFabricacao;
+        }
+
+        public override string EtiquetaPreco()
+        {
+            return
+            NomeProduto + "(Usado), R$" + PrecoProduto + $", (Data Fabricação: {DataFabricacao})";
+        }
+    }
+}
---
+++ EtiquetaProduto/EtiquetaProduto/Program.cs
@@ -0,0 +1,57 @@
+﻿using EtiquetaProduto.Entities;
+
+namespace EtiquetaProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- LISTA DE PRODUTOS ----------");
+            Console.WriteLine();
+
+            List<Produto> produto = new List<Produto>();
+
+            Console.Write("Quantos produtos deseja adicionar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Comum, usado ou importado (c/u/i)? ");
+                string resposta = Console.ReadLine();
+
+                Console.Write("Nome: ");
+                string nomeProduto = Console.ReadLine();
+                Console.Write("Preço: ");
+                double precoProduto = double.Parse(Console.ReadLine());
+
+                if(resposta == "u" || resposta == "U")
+                {
+                    Console.Write("Data Fabricação: ");
+                    DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+                    produto.Add(new ProdutoUsado(nomeProduto, precoProduto, dataFabricacao));
+                }
+                if (resposta == "i" || resposta == "I")
+                {
+                    Console.Write("Taxa Alfandega: ");
+                    double taxaAlfandega = double.Parse(Console.ReadLine());
+
+                    produto.Add(new ProdutoImportado(nomeProduto, precoProduto, taxaAlfandega));
+                }
+                else
+                {
+                    produto.Add(new Produto(nomeProduto, precoProduto));
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Etiquetas de Produtos:");
+            Console.WriteLine();
+
+            foreach(Produto prod in produto)
+            {
+                Console.WriteLine(prod.EtiquetaPreco());
+            }
+        }
+    }
+}
