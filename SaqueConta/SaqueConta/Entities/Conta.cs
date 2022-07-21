e06590674d449b6935e3fa918728dcd85307f656
Alef Melo <alefmelo13@gmail.com>
2022-07-21 19:50:15 -0300

    exercicio proposto - aula de excessões - throw

---
+++ SaqueConta/SaqueConta/Entities/Conta.cs
@@ -0,0 +1,39 @@
+﻿using SaqueConta.Entities.Exceptions;
+
+namespace SaqueConta.Entities
+{
+    class Conta : ApplicationException
+    {
+        public int NumeroConta { get; set; }
+        public string TitularConta { get; set; }
+        public double SaldoConta { get; set; }
+        public double LimiteSaque { get; set; }
+
+        public Conta(int numeroConta, string titularConta, double saldoConta, double limiteSaque)
+        {
+            NumeroConta = numeroConta;
+            TitularConta = titularConta;
+            SaldoConta = saldoConta;
+            LimiteSaque = limiteSaque;
+        }
+
+        public void Deposito(double valorDeposito)
+        {
+            SaldoConta = valorDeposito;
+        }
+
+        public void Saque(double valorSaque)
+        {
+            if (valorSaque > SaldoConta)
+            {
+                throw new DomainException("Saldo insuficiente!");
+            }
+            else if (valorSaque > LimiteSaque)
+            {
+                throw new DomainException("Valor excede limite de saque!");
+            }
+
+            SaldoConta -= valorSaque;
+        }
+    }
+}
---
+++ SaqueConta/SaqueConta/Program.cs
@@ -0,0 +1,49 @@
+﻿using SaqueConta.Entities;
+using SaqueConta.Entities.Exceptions;
+
+namespace SaqueConta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------- SAQUE CONTA ----------");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite os dados da conta:");
+            Console.WriteLine();
+
+            Console.Write("Número: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Titular: ");
+            string titularConta = Console.ReadLine();
+
+            Console.Write("Saldo Inicial: ");
+            double saldoConta = double.Parse(Console.ReadLine());
+
+            Console.Write("Limite de Saque:");
+            double limiteSaque = double.Parse(Console.ReadLine());
+
+            Conta conta = new Conta(numeroConta, titularConta, saldoConta, limiteSaque);
+
+            Console.WriteLine();
+            Console.Write("Digite o valor do saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+
+            try
+            {
+                conta.Saque(valorSaque);
+                Console.WriteLine();
+
+                Console.WriteLine("Novo Saldo: " + conta.SaldoConta);
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
