3682a1c109e476df63fd69bb08bb7636920064b8
Alef Melo <alefmelo13@gmail.com>
2022-07-25 20:33:44 -0300

    salvando

--- LocacaoVeiculo/LocacaoVeiculo/Program.cs
+++ LocacaoVeiculo/LocacaoVeiculo/Program.cs
@@ -6,7 +6,26 @@ namespace LocacaoVeiculo
     {
         void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("---------- ALUGUEL DE CARRO ----------");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite os dados do aluguel:");
+            Console.WriteLine();
+
+            Console.Write("Modelo Carro: ");
+            string modeloCarro = Console.ReadLine();
+            Console.Write("Data retirada(dd/MM/yyyy hh:mm): ");
+            DateTime dataRetirada = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm");
+            Console.Write("Data retorno(dd/MM/yyyy hh:mm): ");
+            DateTime dataRetorno = DateTime.Parse(Console.ReadLine());
+            Console.Write("Preço por hora: ");
+            double valorHora = double.Parse(Console.ReadLine());
+            Console.Write("Preço por dia: ");
+            double valorDia = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
         }
     }
 }
