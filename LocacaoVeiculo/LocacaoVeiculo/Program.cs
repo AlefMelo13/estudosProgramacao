73d26e61c983bf79511c52f4943c18d42a008128
AlefMelo <alefmelo13@gmail.com>
2022-07-26 11:38:33 -0300

    continuação projeto aluguel de veiculo

--- LocacaoVeiculo/LocacaoVeiculo/Program.cs
+++ LocacaoVeiculo/LocacaoVeiculo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using LocacaoVeiculo.Entities;
 
 namespace LocacaoVeiculo
 {
@@ -15,13 +16,15 @@ namespace LocacaoVeiculo
             Console.Write("Modelo Carro: ");
             string modeloCarro = Console.ReadLine();
             Console.Write("Data retirada(dd/MM/yyyy hh:mm): ");
-            DateTime dataRetirada = DateTime.TryParseExact(Console.ReadLine(), "dd/MM/yyyy hh:mm");
+            DateTime dataRetirada = DateTime.Parse(Console.ReadLine());
             Console.Write("Data retorno(dd/MM/yyyy hh:mm): ");
             DateTime dataRetorno = DateTime.Parse(Console.ReadLine());
             Console.Write("Preço por hora: ");
             double valorHora = double.Parse(Console.ReadLine());
             Console.Write("Preço por dia: ");
             double valorDia = double.Parse(Console.ReadLine());
+
+            AluguelCarro aluguelCarro = new AluguelCarro(dataRetirada, dataRetorno, new Veiculo(modeloCarro));
 
             Console.WriteLine();
 
---
+++ LocacaoVeiculo/LocacaoVeiculo/Services/ServicoAluguel.cs
@@ -0,0 +1,21 @@
+﻿using LocacaoVeiculo.Entities;
+
+namespace LocacaoVeiculo.Services
+{
+    public class ServicoAluguel
+    {
+        public double PrecoPorHora { get; private set; }
+        public double PrecoPorDia { get; private set; }
+
+        public ServicoAluguel(double precoPorHora, double precoPorDia)
+        {
+            PrecoPorHora = precoPorHora;
+            PrecoPorDia = precoPorDia;
+        }
+
+        public void ProcessaFatura(AluguelCarro aluguelCarro)
+        {
+
+        }
+    }
+}
