20874bf747d0314fe34ac33c9ac092ac5c4e292d
AlefMelo <alefmelo13@gmail.com>
2022-08-04 15:32:25 -0300

    projeto pedido para treinar habilidades em c# e programação

---
+++ Pedido/Pedido/Entities/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Pedido.Entities
+{
+    public class Pedido
+    {
+        public int NumeroPedido { get; set; }
+        public DateTime DataPedido { get; set; }
+        public Cliente Cliente { get; set; }
+        public static List<Produto> Produto { get; set; }
+
+        public Pedido(int numeroPedido, DateTime dataPedido, Cliente cliente)
+        {
+            NumeroPedido = numeroPedido;
+            DataPedido = dataPedido;
+            Cliente = cliente;
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Cliente: " + Cliente.ToString()
+                 + "Número Pedido: 1"
+                 + "\nData Pedido: " + DateTime.Now
+                 + "\nQuantidade Produtos: "
+                 + "Total do Pedido: ";
+        }
+    }
+}
